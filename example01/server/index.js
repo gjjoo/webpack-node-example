6b15c7859543326a3a3947154adfd6b8c80f463f
gjjoo <joogeunjae@gmail.com>
2018-02-07 18:49:16 +0900

    서버사이드 개발, 프로덕션  분리

--- example01/server/index.js
+++ example01/server/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.dev');
-const webpackMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
 const app = express();
 
 app.use(express.static('client/dist'));
 
-const webpackCompiler = webpack(webpackConfig);
-const wpmw = webpackMiddleware(webpackCompiler, {});
-app.use(wpmw);
+if (process.env.NODE_ENV === 'development') {
+  const webpack = require('webpack');
+  const webpackConfig = require('../webpack.config.dev');
+  const webpackMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
 
-const wphmw = webpackHotMiddleware(webpackCompiler);
-app.use(wphmw);
+  const webpackCompiler = webpack(webpackConfig);
+  const wpmw = webpackMiddleware(webpackCompiler, {});
+  app.use(wpmw);
+
+  const wphmw = webpackHotMiddleware(webpackCompiler);
+  app.use(wphmw);
+}
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
